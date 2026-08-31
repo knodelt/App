fb6f3da3e6e806b0a82d4db973f74bea4bcfd5f3
knodelt <thomasknodel@hotmail.de>
2026-08-31 20:20:00 +0200

    chore: bump app cache for richer cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'frame-v01';
+const CACHE = 'frame-v02';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', event => {
