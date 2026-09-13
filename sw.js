1ba92b90a269fcf0ca6e4c434d8d8f41c6a1689f
knodelt <thomasknodel@hotmail.de>
2026-09-13 17:40:54 +0200

    Bump PWA cache for refined card details

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'frame-v05';
+const CACHE = 'frame-v06';
 const ASSETS = ['/', '/index.html', '/about.html', '/styles.css', '/app.js', '/tmdb-feed.js', '/card-details.js', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', event => {
