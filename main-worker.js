6c323466cdb3aaa178f53949b427ff1493074cf5
knodelt <thomasknodel@hotmail.de>
2026-09-16 18:47:33 +0200

    Route personalized recommendations API

--- main-worker.js
+++ main-worker.js
@@ -1,11 +1,15 @@
 import baseWorker from './worker.js';
 import { handleAvailability } from './availability.js';
+import { handlePersonalizedRecommendations } from './recommendations-api.js';
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     if (url.pathname === '/api/availability') {
       return handleAvailability(request, env);
+    }
+    if (url.pathname === '/api/recommendations') {
+      return handlePersonalizedRecommendations(request, env);
     }
     return baseWorker.fetch(request, env, ctx);
   }
